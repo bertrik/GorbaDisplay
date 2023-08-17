6df01d7e362a704f7073eafb88db224a09f4cf71
Folkert van Heusden <mail@vanheusden.com>
2023-08-17 15:57:01 +0200

    tabs => spaces

--- pixelflood.h
+++ pixelflood.h
@@ -9,35 +9,35 @@
 class pixelflood
 {
 private:
-	const int listen_port_ascii;
-	const int listen_port_binary;
-	const int width;
-	const int height;
-	bool    (* draw_pixel )(int x, int y, uint8_t gray);
-	uint8_t (* rgb_to_gray)(uint8_t r, uint8_t g, uint8_t b);
+    const int listen_port_ascii;
+    const int listen_port_binary;
+    const int width;
+    const int height;
+    bool    (* draw_pixel )(int x, int y, uint8_t gray);
+    uint8_t (* rgb_to_gray)(uint8_t r, uint8_t g, uint8_t b);
 
-	uint32_t last_announcement { 0 };
+    uint32_t last_announcement { 0 };
 
-	WiFiUDP port_ascii;
-	WiFiUDP port_binary;
+    WiFiUDP port_ascii;
+    WiFiUDP port_binary;
 
-	void poll_ascii();
+    void poll_ascii();
 
-	void draw_binary_0(const uint8_t *const start, const uint8_t *const end, const bool alpha);
-	void draw_binary_1(const uint8_t *const start, const uint8_t *const end, const bool alpha);
-	void draw_binary_2(const uint8_t *const start, const uint8_t *const end, const bool alpha);
-	void draw_binary_3(const uint8_t *const start, const uint8_t *const end, const uint8_t color);
-	void poll_binary();
+    void draw_binary_0(const uint8_t *const start, const uint8_t *const end, const bool alpha);
+    void draw_binary_1(const uint8_t *const start, const uint8_t *const end, const bool alpha);
+    void draw_binary_2(const uint8_t *const start, const uint8_t *const end, const bool alpha);
+    void draw_binary_3(const uint8_t *const start, const uint8_t *const end, const uint8_t color);
+    void poll_binary();
 
 public:
-	pixelflood(const int listen_port_ascii,
-			const int listen_port_binary,
-			const int width, const int height,
-			bool (*draw_pixel)(int x, int y, uint8_t gray),
-			uint8_t (* rgb_to_gray)(uint8_t r, uint8_t g, uint8_t b));
-	virtual ~pixelflood();
+    pixelflood(const int listen_port_ascii,
+            const int listen_port_binary,
+            const int width, const int height,
+            bool (*draw_pixel)(int x, int y, uint8_t gray),
+            uint8_t (* rgb_to_gray)(uint8_t r, uint8_t g, uint8_t b));
+    virtual ~pixelflood();
 
-	bool begin();
+    bool begin();
 
-	void poll();
+    void poll();
 };
